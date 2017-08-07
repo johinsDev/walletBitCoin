dfff3a5df983d5d423bf5a65e77776f3380aaac1
johinsdev <johinsdev@gmail.com>
2017-08-07 15:08:10 -0500

    --amend

--- app/controllers/wallets.js
+++ app/controllers/wallets.js
@@ -20,7 +20,23 @@ const create = (req, res, next) => {
         return res.json(wallet);
     });
 }
-
+//crear la transaccion si es exitosa
+//entonces generar un pago en la tabl con el valor con el costo y otros datos q responda block io
+// ID	e03889fd-c573-4b30-afe7-863fec6ac125
+// Destinatario	2Mzy8Eskyi92F7ByLiT9EvozTxFyvVUgbUS
+// Crypto Txid	b4e8e6ceeff688d5fa79c9a27376091daf4eea49309342f9379a7b8769891a90
+// Monto	0 BTC
+// Costo	0.00097053 BTC
+// Total	0.01 BTC
+// Estado	Completado
+// revisar tabla comisiones para las transacciones que hace mario
+// url payments o pagos donde los liste
+// url transactions coon los ingresos
+// summary que regresa los datos del resumen
+//son unas cuantas consultas
+// logout caducar el token
+//refresh token
+//luego mirar como actulizar nuestra tabla de wallets
 
 const show = (req, res) => {
     blockIo.get_address_by_label({'label': req.params.id}, (err, wallet) => {
