50bd75a8dfe76b8c8aad752f4cf9049d0b65991a
johinsdev <johinsdev@gmail.com>
2017-08-14 23:48:01 -0500

    create view stats

--- app/models/blockIo.js
+++ app/models/blockIo.js
@@ -1,5 +1,5 @@
 import BlockIo from 'block_io';
-const blockIO = new BlockIo('c923-24f1-d8c2-fbf9','maleja280516', 2);
+const blockIO = new BlockIo('3740-59bd-4f5c-d1d4','maleja280516', 2);
 export default function blockIo(){
     return blockIO;
 }
